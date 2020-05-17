afc63399ece0a3617b498b319584785f92aa697e
blackroostercouk <65395900+blackroostercouk@users.noreply.github.com>
2020-05-17 14:14:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -421,7 +421,7 @@ $(".workcollection").find(".collectioncontainer").eq(1).addClass("ters");
 $( ".collectioncontainer" ).each(function( index ) {
      $(this).addClass('cl'+index+'');
      $(this).find(".cub2").find(".cub").attr('id','gb'+index+'');
-     $(this).find(".collectionleft").find(".cub2").find(".cub").find("img").attr('id','im'+index+'');
+     $(this).find(".cub2").find(".cub").find("img").attr('id','im'+index+'');
      
 });
 
