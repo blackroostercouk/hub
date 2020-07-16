750b04bced0abb33f1ca894a0c65c8ce11b0e483
blackroostercouk <65395900+blackroostercouk@users.noreply.github.com>
2020-07-16 16:48:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -618,4 +618,541 @@ leaveButton99();
 
 
 
+var md = new MobileDetect(window.navigator.userAgent);
+
+$(".bol4").hide();
+$(".leftarrow").hide();
+$(".rightarrow").hide();
+
+$(".leftarrow").css("transform", "rotate(90deg)");
+$(".rightarrow").css("transform", "rotate(-90deg)");
+
+anime.remove('.iitem');
+anime({
+  targets: '.iitem',
+  scaleX: 1.4,
+  scaleY: 1.4,
+  duration:0,
+ });
+
+
+
+
+
+function leftSide(translateX) {
+anime.remove('.bol');
+anime({
+  targets: '.bol',
+  translateX: translateX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  
+}
+
+
+function crve1(scale,translateY,color) {
+anime.remove('.bol4'); 
+anime({
+  targets: '.bol4',
+  scale: scale,
+  translateY: translateY,
+  color: color,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  
+}
+
+function otext(scale2) {
+anime.remove('.bol2');
+anime({
+  targets: '.bol2',
+  scale: scale2,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  
+}
+
+function rightS(translateX) {
+anime.remove('.bol3');
+anime({
+  targets: '.bol3',
+  translateX: translateX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  
+}
+
+
+
+
+function enterButtonAlt() { leftSide(-10) };
+function leaveButtonAlt() { leftSide(0) };
+
+function rightSEAlt() { rightS(10) };
+function rightSEopAlt() { rightS(0) };
+
+function ortextAlt() { otext(0.9) };
+function otextopAlt() { otext(1) };
+
+function curve1Alt() { crve1(1.3,-5,['#1E1E1E', '#FFC200']) };
+function curve1opAlt() { crve1(1,0,['#FFC200', '#1E1E1E']) };
+
+function enterButton() { leftSide(-20) };
+function leaveButton() { leftSide(0) };
+
+function rightSE() { rightS(20) };
+function rightSEop() { rightS(0) };
+
+function ortext() { otext(0.8) };
+function otextop() { otext(1) };
+
+function curve1() { crve1(1.4,-15,['#1E1E1E', '#FFC200']) };
+function curve1op() { crve1(1,0,['#FFC200', '#1E1E1E']) };
+
+
+
+
+
+
+
+
+
+$(".itemmenu").mouseenter(function(){
+if(md.mobile() != null)
+{
+$(".bol4").show();  
+enterButtonAlt();
+curve1Alt();
+ortextAlt();
+rightSEAlt();
+}else{
+$(".bol4").show();  
+enterButton();
+curve1();
+ortext();
+rightSE();
+}
+
+});
+
+
+$(".itemmenu").mouseleave(function(){
+if(md.mobile() != null)
+{
+$(".bol4").hide();  
+leaveButtonAlt();
+curve1opAlt(); 
+otextopAlt();
+rightSEopAlt(); 
+}else{
+$(".bol4").hide();  
+leaveButton();
+curve1op(); 
+otextop();
+rightSEop(); 
+}
+ 
+});
+
+
+
+function item1Anim(translateX) {
+anime.remove('.item1');
+anime({
+  targets: '.item1',
+  translateX: translateX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  /*-40*/
+}
+
+
+function item1EnterAlt() { item1Anim(-4) };
+function item1LeaveAlt() { item1Anim(0) };
+
+function item1Enter() { item1Anim(-40) };
+function item1Leave() { item1Anim(0) };
+
+
+function item2Anim(rotate,scaleX,scaleY) {
+anime.remove('.iitem');
+anime({
+  targets: '.iitem',
+  rotate: rotate,
+  easing: 'easeOutQuint',
+  duration: 300,
+  scaleX: scaleX,
+  scaleY: scaleY,
+  delay:60,
+ });
+ 
+}
+
+function item2Enter() { item2Anim(90 , [1.4 , 0.8] , [1.4 , 0.8]) };
+function item2Leave() { item2Anim(0 ,  [0.8 , 1.4] , [0.8 , 1.4]) };
+
+function item3Anim(translateX) {
+anime.remove('.item3');
+anime({
+  targets: '.item3',
+  translateX: translateX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  /*50*/
+}
+
+function item3EnterAlt() { item3Anim(14) };
+function item3LeaveAlt() { item3Anim(0) };
+
+function item3Enter() { item3Anim(50) };
+function item3Leave() { item3Anim(0) };
+
+
+function leftArrowAnim(opacity,transformX) {
+anime.remove('.leftarrow');
+anime({
+  targets: '.leftarrow',
+  opacity: opacity,
+  translateY: transformX,
+  easing: 'easeOutQuint',
+  duration: 500,
+ });
+  /*[0,1] [100,0] */
+}
+
+function leftArrowEnter() { leftArrowAnim([0,1],[0,50]) };
+function leftArrowLeave() { leftArrowAnim([1,0],[50,0]) };
+
+function rightArrowAnim(opacity,transformX) {
+anime.remove('.rightarrow');
+anime({
+  targets: '.rightarrow',
+  opacity: opacity,
+  translateY: transformX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  /*[0,1] [100,0] */
+}
+
+function rightArrowEnter() { rightArrowAnim([0,1],[0,60]) };
+function rightArrowLeave() { rightArrowAnim([0,1],[60,0]) };
+
+///önemli
+$(".itemmenu2").mouseenter(function(){ 
+if(md.mobile() != null)
+{
+$(".leftarrow").show();
+$(".rightarrow").show(); 
+item1EnterAlt();
+item2Enter();
+item3EnterAlt();
+leftArrowEnter();
+rightArrowEnter();
+console.log('mobile work');
+}else{
+$(".leftarrow").show();
+$(".rightarrow").show(); 
+item1Enter();
+item2Enter();
+item3Enter();
+leftArrowEnter();
+rightArrowEnter();
+console.log('desktop work');
+}
+
+});
+
+
+$(".itemmenu2").mouseleave(function(){
+$(".leftarrow").hide();
+$(".rightarrow").hide();    
+item1Leave();
+item2Leave();
+item3Leave();
+leftArrowLeave();
+rightArrowLeave();
+});
+
+
+
+function piece2M(scale) {
+anime.remove('.piece2');
+anime({
+  targets: '.piece2',
+  scale: scale,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  /*[1,1.5] */
+}
+
+function piece2MEnter() { piece2M([1,1.5]) };
+function piece2MLeave() { piece2M([1.5,1]) };
+
+/*anime({
+  targets: '.piece2',
+  scale: [1,1.5],
+ });*/
+function piece1M(translateX) {
+anime.remove('.piece1');
+anime({
+  targets: '.piece1',
+  translateX: translateX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+  /*[0,-30] */
+}
+
+function piece1MEnterAlt() { piece1M([0,-10]) };
+function piece1MLeaveAlt() { piece1M([-10,0]) };
+
+function piece1MEnter() { piece1M([0,-30]) };
+function piece1MLeave() { piece1M([-30,0]) };
+
+
+function piece3M(translateX) {
+anime.remove('.piece3');
+anime({
+  targets: '.piece3',
+  translateX: translateX,
+  easing: 'easeOutQuint',
+  duration: 500
+ });
+
+}
+
+
+function piece3MEnterAlt() { piece3M([0,10]) };
+function piece3MLeaveAlt() { piece3M([10,0]) };
+
+function piece3MEnter() { piece3M([0,30]) };
+function piece3MLeave() { piece3M([30,0]) };
+
+
+$(".itemmenu3").mouseenter(function(){ 
+if(md.mobile() != null)
+{
+  piece1MEnterAlt();
+  piece2MEnter();
+  piece3MEnterAlt();
+}else{
+  piece1MEnter();
+  piece2MEnter();
+  piece3MEnter();
+}
+
+});
+
+
+$(".itemmenu3").mouseleave(function(){
+if(md.mobile() != null)
+{
+  piece1MLeaveAlt();
+  piece2MLeave();
+  piece3MLeaveAlt();
+  }
+  else
+  {
+  piece1MLeave();
+  piece2MLeave();
+  piece3MLeave();
+  }
+});
+
+
+$( ".collectioncontainer" ).each(function( index ) {
+     $(this).addClass('cl'+index+'');
+     $(this).find(".cub2").find(".cub").attr('id','gb'+index+'');
+     $(this).find(".cub2").find(".cub").find(".embedt").attr('id','im'+index+'');
+     $(this).find(".cub2").find(".plancvf").attr('id','dr'+index+'');	
+     
+});
+
+
+
+$(".cl0").mouseenter(function(){
+anime.remove('#gb0');
+anime.remove('#dr0');
+anime.remove('#im0');
+  anime({
+      targets: '#gb0',
+      height:15,
+      easing: 'easeOutQuint',
+      duration:500,
+     });
+     anime({
+      targets: '#gb0',
+      easing: 'easeOutQuint',
+      scale: [1,4],
+      borderRadius: ['0%' , '50%'],
+      delay:500,
+      duration:500,
+     });
+     anime({
+      targets: '#im0',
+      opacity: [0, 1],
+      easing: 'easeOutQuint',
+      delay:500,
+      duration:500,
+     });
+     anime({
+      targets:'#dr0',
+      height:15,
+      easing: 'easeOutQuint',
+      opacity:[1,0],
+      duration:500,
+     });
+     anime({
+      targets: '#dr0',
+      easing: 'easeOutQuint',
+      scale: [1,5],
+      borderRadius: ['0%' , '50%'],
+      duration:500,
+      delay:500,
+      opacity:0,
+     });
+     anime({
+      targets: '#dr0',
+      opacity:[1,0],
+      duration:1500,
+      scale: [1,8],
+      backgroundColor: ['#1e1e1e','#f8f8f8'],
+      delay:1000,
+      easing: 'easeOutQuint',
+      //autoplay:false,
+      loop:true, 
+        });
+});
+
+$(".cl0").mouseleave(function(){
+anime.remove('#gb0');
+anime.remove('#dr0');
+anime.remove('#im0');
+  anime({
+      targets: '#im0',
+      opacity: [1,0],
+      easing: 'easeOutQuint',
+      duration:500,
+     });
+     anime({
+      targets: '#gb0',
+      easing: 'easeOutQuint',
+      scale: [4,1],
+      borderRadius: ['50%' , '0%'],
+      duration:500,
+     });
+   anime({
+      targets: '#gb0',
+      height: 350,
+      easing: 'easeOutQuint',
+      delay:1000,
+      duration:500,
+     });
+     
+      anime({
+      targets: '#dr0',
+      opacity:0,
+      duration:500,
+      scale: 1,
+      easing: 'easeOutQuint',
+      });
+});
+
+
+$(".cl1").mouseenter(function(){
+anime.remove('#gb1');
+anime.remove('#dr1');
+anime.remove('#im1');
+  anime({
+      targets: '#gb1',
+      height:15,
+      easing: 'easeOutQuint',
+      duration:500,
+     });
+     anime({
+      targets: '#gb1',
+      easing: 'easeOutQuint',
+      scale: [1,4],
+      borderRadius: ['0%' , '50%'],
+      delay:500,
+      duration:500,
+     });
+     anime({
+      targets: '#im1',
+      opacity: [0, 1],
+      easing: 'easeOutQuint',
+      delay:500,
+      duration:500,
+     });
+     anime({
+      targets:'#dr1',
+      height:15,
+      easing: 'easeOutQuint',
+      opacity:[1,0],
+      duration:500,
+     });
+     anime({
+      targets: '#dr1',
+      easing: 'easeOutQuint',
+      scale: [1,5],
+      borderRadius: ['0%' , '50%'],
+      duration:500,
+      delay:500,
+      opacity:0,
+     });
+     anime({
+      targets: '#dr1',
+      opacity:[1,0],
+      duration:1500,
+      scale: [1,8],
+      backgroundColor: ['#1e1e1e','#f8f8f8'],
+      delay:1000,
+      easing: 'easeOutQuint',
+      //autoplay:false,
+      loop:true, 
+        });
+});
+
+$(".cl1").mouseleave(function(){
+anime.remove('#gb1');
+anime.remove('#dr1');
+anime.remove('#im1');
+  anime({
+      targets: '#im1',
+      opacity: [1,0],
+      easing: 'easeOutQuint',
+      duration:500,
+     });
+     anime({
+      targets: '#gb1',
+      easing: 'easeOutQuint',
+      scale: [4,1],
+      borderRadius: ['50%' , '0%'],
+      duration:500,
+     });
+   anime({
+      targets: '#gb1',
+      height: 350,
+      easing: 'easeOutQuint',
+      delay:1000,
+      duration:500,
+     });
+     
+      anime({
+      targets: '#dr1',
+      opacity:0,
+      duration:500,
+      scale: 1,
+      easing: 'easeOutQuint',
+      });
+});
+
 
