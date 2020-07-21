850ce0f3562e6e3fd5b9cb6e9567c36a4e5548d6
blackroostercouk <65395900+blackroostercouk@users.noreply.github.com>
2020-07-21 16:05:46 +0100

    Update openingAnimation.js

--- openingAnimation.js
+++ openingAnimation.js
@@ -57,3 +57,64 @@ tl2222.add({
    targets: '.ddddd',
    rotate: '360deg',
 });
+
+
+var tl11111 = anime.timeline({
+   duration: 1500,
+   easing: 'easeInQuint',
+   autoplay:true,
+});
+
+var tl22222 = anime.timeline({
+   duration: 1500,
+   easing: 'easeInQuint',
+   autoplay:true,
+});
+
+
+tl11111.add({
+  targets: '#svg_22222',
+  easing: 'easeInOutSine',
+  fill:'#f8f8f8',
+  stroke: 'rgba(0,0,0,1)',
+  strokeWidth: '1px',
+  strokeDashoffset: [anime.setDashoffset, 0],
+}).add({
+  targets: '#svg_22222',
+  fill:'#1e1e1e',
+  stroke: 'rgba(0,0,0,0)',
+  duration: 500,
+}).add({
+  targets: '#svg_22222',
+  fill:'#1e1e1e',
+  stroke: 'rgba(0,0,0,0)',
+  duration: 1000,
+});
+
+tl22222.add({
+  targets: '#svg_33333',
+  easing: 'easeInOutSine',
+  fill:'#f8f8f8',
+  stroke: 'rgba(0,0,0,1)',
+  strokeWidth: '1px',
+  strokeDashoffset: [anime.setDashoffset, 0],
+}).add({
+  targets: '#svg_33333',
+  fill:'#1e1e1e',
+  stroke: 'rgba(0,0,0,0)',
+  duration: 500,
+}).add({
+  targets: '#svg_33333',
+  fill:'#1e1e1e',
+  stroke: 'rgba(0,0,0,0)',
+  duration: 1000,
+}).add({
+   targets: '.dddddd',
+   rotate: '360deg',
+});
+
+
+setInterval(function(){ 
+     tl11111.restart();
+     tl22222.restart();
+}, 20000);
